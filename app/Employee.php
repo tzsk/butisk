bae2f55a49ff6048512d4262594c2264a7b32308
tzsk <mailtokmahmed@gmail.com>
2020-04-27 00:59:21 +0530

    Some cleanup

--- app/Employee.php
+++ app/Employee.php
@@ -3,9 +3,19 @@
 namespace App;
 
 use App\Contracts\HasNovaAccess;
+use Carbon\Carbon;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * @property string name
+ * @property string email
+ * @property string password
+ * @property string remember_token
+ * @property Carbon email_verified_at
+ * @property Carbon created_at
+ * @property Carbon updated_at
+ */
 class Employee extends Authenticatable implements HasNovaAccess
 {
     use Notifiable;
